5b057c12bb89ec0fa7d53ae5829c88a752b3a00b
Naveen Kumar <navuniverse@gmail.com>
2018-10-12 14:10:18 +0530

    Corercted vault config

--- src/main/java/com/rockingengineering/vault/VaultApplication.java
+++ src/main/java/com/rockingengineering/vault/VaultApplication.java
@@ -2,13 +2,12 @@ package com.rockingengineering.vault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
 public class VaultApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaultApplication.class, args);
 	}
+
 }
--- src/main/java/com/rockingengineering/vault/controller/VaultController.java
+++ src/main/java/com/rockingengineering/vault/controller/VaultController.java
@@ -3,15 +3,13 @@
  */
 package com.rockingengineering.vault.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
-import java.util.Set;
-
-import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
 
 /**
  * 
@@ -19,34 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @date 23-Oct-2017
  */
+@RefreshScope
 @Api(value = "Vault Controller")
 @RestController
-@RequestMapping("api")
 public class VaultController {
 
-	private static final Logger logger = Logger.getLogger(VaultController.class);
+	@Value("${client.pseudo.property}")
+	private String pseudoProperty;
 
-	@ApiOperation(value = "Get All Vault Keys")
-	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
-	public Set<String> getAllMapKeys() {
-		logger.debug("Received Get All Vault Keys Request");
+	@Value("${client.pseudo.property}")
+	private String proeprtyFromVault;
 
-		return null;
+	@GetMapping("/property")
+	public ResponseEntity<String> getProperty() {
+		return ResponseEntity.ok(pseudoProperty);
 	}
 
-	@ApiOperation(value = "Add key to Vault")
-	@RequestMapping(value = "/addKey", method = RequestMethod.POST)
-	public String addKeyToVault(String key, String value) {
-		logger.debug("Received Add key to Vault Request");
-
-		return "Added Key: " + key + " in Vault";
-	}
-
-	@ApiOperation(value = "Remove key to Vault")
-	@RequestMapping(value = "/removeKey", method = RequestMethod.POST)
-	public String removeKeyFromMap(String key) {
-		logger.debug("Received Remove key from Vault Request");
-
-		return "Removed Key: " + key + " from vault";
+	@GetMapping("/property/vault")
+	public ResponseEntity<String> getPropertyFromVault() {
+		return ResponseEntity.ok(proeprtyFromVault);
 	}
 }
